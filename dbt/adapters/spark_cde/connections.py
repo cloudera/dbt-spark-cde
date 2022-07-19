0ef7e78c5fb6b74f4b582cbf235579d90544fb82
V. Ganesh <tovganesh@yahoo.co.in>
2022-07-19 23:08:17 +0530

    rename adapter name exposed in connections.py to spark_cde (#8)
    Co-authored-by: V. Ganesh <ganesh.venkateshwara@cloudera.com>

--- dbt/adapters/spark_cde/connections.py
+++ dbt/adapters/spark_cde/connections.py
@@ -157,7 +157,7 @@ class SparkCredentials(Credentials):
 
     @property
     def type(self):
-        return "spark"
+        return "spark_cde"
 
     @property
     def unique_field(self):
@@ -292,7 +292,7 @@ class PyodbcConnectionWrapper(PyhiveConnectionWrapper):
 
 
 class SparkConnectionManager(SQLConnectionManager):
-    TYPE = "spark"
+    TYPE = "spark_cde"
 
     SPARK_CLUSTER_HTTP_PATH = "/sql/protocolv1/o/{organization}/{cluster}"
     SPARK_SQL_ENDPOINT_HTTP_PATH = "/sql/1.0/endpoints/{endpoint}"
